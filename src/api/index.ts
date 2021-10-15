ac58c2b259b2b09b063564a3834d4f7b5dd08604
gyanendrav2 <gyanendrav2@gmail.com>
2021-10-15 11:32:05 +0530

    added the cors proxy cors anywhere

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 
+const PROXY = window.location.hostname === "localhost"
+  ? "https://cors-anywhere.herokuapp.com"
+  : "/cors-proxy";
+
 export const searchAPIcall = async (query: string) => {
   try {
     const result = await axios(
-      `http://suggestqueries.google.com/complete/search?client=firefox&q=${query}`
+      `${PROXY}/https://suggestqueries.google.com/complete/search?client=firefox&q=${query}`
     );
     return result;
   } catch (err) {
